c4d0c373b8b92cb6f1f40cb0be119144b37e86f6
Amol Gosavi <amol@watconsult.com>
2022-02-25 15:33:37 +0530

    new slider added

--- js/toyota-connect/toyota_i-connect.js
+++ js/toyota-connect/toyota_i-connect.js
@@ -51,6 +51,24 @@ $(document).ready(function () {
       },
     });
   }
+
+  $(".connectTo").owlCarousel({
+    loop: false,
+    margin: 10,
+    nav: false,
+    dots: true,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 2,
+      },
+      1000: {
+        items: 3,
+      },
+    },
+  });
 });
 
 $(window).resize(function () {
